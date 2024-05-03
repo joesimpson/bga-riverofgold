2c50421e75b6be4d4476d700ab3744e13b91d729
joesimpson <1324811+joesimpson@users.noreply.github.com>
2024-05-03 15:28:35 +0200

    [UI] New setting new pin side panel when scrolling

--- riverofgold.js
+++ riverofgold.js
@@ -214,6 +214,8 @@ function (dojo, declare) {
             this._inactiveStates = ['draft'];
             
             this._hideNotifsWhenMultiActive = true;
+            
+            this.sidePanelPinned = false;
         },
         
         ///////////////////////////////////////////////////
@@ -289,6 +291,7 @@ function (dojo, declare) {
         getSettingsSections: ()=>({
             layout: _("Layout"),
             buttons: _("Buttons"),
+            sidePanel: _("Side panel"),
         }),
         getSettingsConfig() {
             let gameWidthScale = this.getGameZoneWidth()/1200;
@@ -379,9 +382,16 @@ function (dojo, declare) {
                     },
                   },
                 }, 
-                playerPanelDetails: { section: "layout", type: 'pref', prefId: PREF_PLAYER_PANEL_DETAILS },
+                //sidePanelPinned: {
+                //    section: "sidePanel",
+                //    default: 0,
+                //    name: _('Pin on scroll'),
+                //    type: 'switch',
+                //    attribute: 'rog_pin_panel',
+                //}, 
+                playerPanelDetails: { section: "sidePanel", type: 'pref', prefId: PREF_PLAYER_PANEL_DETAILS },
                 logTileWidth: {
-                    section: "layout",
+                    section: "sidePanel",
                   default: 50,
                   name: _('Tile width in logs'),
                   type: 'slider',
@@ -409,6 +419,7 @@ function (dojo, declare) {
                       },
                     },
                 }, 
+
             };
         },
         
@@ -1321,6 +1332,13 @@ function (dojo, declare) {
             });
         },
         
+        togglePinMode(check){
+            let newValue = check ? true : false;
+            this.sidePanelPinned = newValue;
+            $('ebd-body').setAttribute('data-rog_pin_panel', newValue);
+            //$('pin-panel-switch').setAttribute('data-pinned', newValue);
+        },
+        
         undoToStep(stepId) {
             this.checkAction('actRestart');
             this.takeAction('actUndoToStep', { stepId }, false);
@@ -1691,7 +1709,19 @@ function (dojo, declare) {
             let chk = $('help-mode-chk');
             dojo.connect(chk, 'onchange', () => this.toggleHelpMode(chk.checked));
             this.addTooltip('help-mode-switch', '', _('Toggle help/safe mode.'));
-            
+
+            //$('pin-panel-switch').addEventListener('click', () => {
+            //   let oldValue = this.settings.sidePanelPinned;
+            //   let newValue = (oldValue == 1) ? 0 :1;
+            //   this.settings.sidePanelPinned = newValue;
+            //   $('ebd-body').setAttribute('data-rog_pin_panel', newValue);
+            //   $('pin-panel-switch').setAttribute('data-pinned', newValue);
+            // });
+
+            let chkPin = $('pin-panel-chk');
+            dojo.connect(chkPin, 'onchange', () => this.togglePinMode(chkPin.checked));
+            this.addTooltip('pin-panel-switch', '', _('Pin on Scrolling'));
+        
             this._settingsModal = new customgame.modal('showSettings', {
                 class: 'rog_popin',
                 closeIcon: 'fa-times',
@@ -1724,7 +1754,13 @@ function (dojo, declare) {
                         </label>
                         <svg aria-hidden="true" focusable="false" data-prefix="fad" data-icon="question-circle" class="svg-inline--fa fa-question-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g class="fa-group"><path class="fa-secondary" fill="currentColor" d="M256 8C119 8 8 119.08 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 422a46 46 0 1 1 46-46 46.05 46.05 0 0 1-46 46zm40-131.33V300a12 12 0 0 1-12 12h-56a12 12 0 0 1-12-12v-4c0-41.06 31.13-57.47 54.65-70.66 20.17-11.31 32.54-19 32.54-34 0-19.82-25.27-33-45.7-33-27.19 0-39.44 13.14-57.3 35.79a12 12 0 0 1-16.67 2.13L148.82 170a12 12 0 0 1-2.71-16.26C173.4 113 208.16 90 262.66 90c56.34 0 116.53 44 116.53 102 0 77-83.19 78.21-83.19 106.67z" opacity="0.4"></path><path class="fa-primary" fill="currentColor" d="M256 338a46 46 0 1 0 46 46 46 46 0 0 0-46-46zm6.66-248c-54.5 0-89.26 23-116.55 63.76a12 12 0 0 0 2.71 16.24l34.7 26.31a12 12 0 0 0 16.67-2.13c17.86-22.65 30.11-35.79 57.3-35.79 20.43 0 45.7 13.14 45.7 33 0 15-12.37 22.66-32.54 34C247.13 238.53 216 254.94 216 296v4a12 12 0 0 0 12 12h56a12 12 0 0 0 12-12v-1.33c0-28.46 83.19-29.67 83.19-106.67 0-58-60.19-102-116.53-102z"></path></g></svg>
                     </div>
-
+                    <div id="pin-panel-switch">
+                        <input type="checkbox" class="checkbox" id="pin-panel-chk" />
+                        <label class="label" for="pin-panel-chk">
+                            <div class="ball"></div>
+                        </label>
+                        <div class='pin-panel-icon'><i class="fa6 fa6-thumbtack"></i></div>
+                    </div>
                     <div id="show-settings">
                     <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
                         <g>
