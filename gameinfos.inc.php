9ef353a49946576dc823039769921a007420a0bb
joesimpson <1324811+joesimpson@users.noreply.github.com>
2025-01-09 15:16:11 +0100

    [CLEAN] obsolete game infos

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -108,13 +108,5 @@ $gameinfos = array(
 //Disable 3d
 'enable_3d' => false,
 
-//////// BGA SANDBOX ONLY PARAMETERS (DO NOT MODIFY)
-
-// simple : A plays, B plays, C plays, A plays, B plays, ...
-// circuit : A plays and choose the next player C, C plays and choose the next player D, ...
-// complex : A+B+C plays and says that the next player is A+B
-'is_sandbox' => false,
-'turnControl' => 'simple'
-
 ////////
 );
