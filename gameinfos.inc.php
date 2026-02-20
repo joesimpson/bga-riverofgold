95dfc177eb4b165bf9fbf6e4152ff0d3ee619c22
joesimpson <1324811+joesimpson@users.noreply.github.com>
2026-02-20 16:36:49 +0100

    [CLEAN] Deprecated keys

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -66,9 +66,6 @@ $gameinfos = array(
 // Allow to rank solo games for games where it's the only available mode (ex: Thermopyles). Should be left to false for games where solo mode exists in addition to multiple players mode.
 'solo_mode_ranked' => false,
 
-// Game is "beta". A game MUST set is_beta=1 when published on BGA for the first time, and must remains like this until all bugs are fixed.
-'is_beta' => 1,                     
-
 // Is this game cooperative (all players wins together or loose together)
 'is_coop' => 0,
 
@@ -104,9 +101,6 @@ $gameinfos = array(
     //  minimum possible value: 740
     'max' => null
 ),
-
-//Disable 3d
-'enable_3d' => false,
 
 ////////
 );
